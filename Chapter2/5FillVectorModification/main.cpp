05caeb188882163fb6ae596269d144eec8b6540d
petkovgogo <petkovgogo@gmail.com>
2020-11-13 11:18:11 +0200

    Added Chapter 2 fifth task

---
+++ Chapter2/5FillVectorModification/main.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+    std::vector<std::string> v;
+    std::ifstream in("dummy.txt");
+    std::string line;
+
+    while (getline(in, line))
+    {
+        v.push_back(line);
+    }
+
+    for (int i = v.size() - 1; i >= 0; i--)
+    {
+        std::cout << i << ": " << v[i] << std::endl;
+    }
+}
