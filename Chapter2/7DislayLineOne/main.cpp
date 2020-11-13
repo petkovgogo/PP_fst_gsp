d23bbd3d0772519981cba8a169f44aeb47fa906b
petkovgogo <petkovgogo@gmail.com>
2020-11-13 11:30:14 +0200

    Added Chapter 2 seventh task

---
+++ Chapter2/7DislayLineOne/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+int main()
+{
+    std::vector<std::string> v;
+    std::ifstream in("dummy.txt");
+    std::string line;
+
+    while (getline(in, line))
+    {
+        v.push_back(line);
+    }
+
+    for (int i = 0; i < v.size(); i++)
+    {
+        std::cin.get();
+        std::cout << i << ": " << v[i] << std::endl;
+    }
+}
