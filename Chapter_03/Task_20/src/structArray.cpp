27a2a7e16364c0319c45f6f57f0c0717c12e25d1
petkovgogo <petkovgogo@gmail.com>
2020-11-26 15:50:00 +0200

    Added Chapter 3 Task 20 #30

---
+++ Chapter_03/Task_20/src/structArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define SIZE 10
+#define PRINT_ELEMENT_ADDRESSES(TYPE, ARR)\
+    std::cout << "sizeof("#TYPE") = " << sizeof(TYPE) << std::endl;\
+    for (int i = 0; i < SIZE; i++)\
+    {\
+        std::cout << "&arr[" << i << "] = " << (long)&ARR[i] << std::endl;\
+    }\
+    \
+    std::cout << std::endl; 
+
+typedef struct
+{
+    int i, j, k;
+} ThreeDpoint;
+
+int main()
+{
+    ThreeDpoint p[SIZE];
+
+    PRINT_ELEMENT_ADDRESSES(ThreeDpoint, p);
+
+    return 0;
+}
