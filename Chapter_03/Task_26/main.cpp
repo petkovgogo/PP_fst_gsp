f7e42f72d8dda88b465b508ee6d0a1453b198d03
petkovgogo <petkovgogo@gmail.com>
2020-11-27 12:07:58 +0200

    Fixed Chapter 3 Task 26 #36

--- Chapter_03/Task_26/main.cpp
+++ Chapter_03/Task_26/main.cpp
@@ -3,9 +3,9 @@
 #define SIZE 8
 #define BYTE_RANGE 16
 #define SUBSTITUTION_VALUE 42
-#define PRINT_ARR(VAR)              \
+#define PRINT_ARR(VAR, LENGTH)              \
     std::cout << #VAR" = [";         \
-    for (int i = 0; i < SIZE; i++)  \
+    for (int i = 0; i < LENGTH; i++)  \
     {                               \
         std::cout << " " << VAR[i]; \
     }                               \
@@ -18,20 +18,20 @@ void setBytes(void *vpArr, int byteRange, int value)
 
     for (int i = 0; i < byteRange; i++)
     {
-        cpArr[i] = value - i;
+        cpArr[i] = value + i * 2;
     }
 
-    PRINT_ARR(cpArr);
+    PRINT_ARR(cpArr, byteRange);
 }
 
 int main()
 {
     int arr[SIZE]{0};
-    PRINT_ARR(arr);
+    PRINT_ARR(arr, SIZE);
 
     void *pArr = static_cast<void *>(arr);
     setBytes(pArr, BYTE_RANGE, SUBSTITUTION_VALUE);
 
-    PRINT_ARR(arr);
+    PRINT_ARR(arr, SIZE);
     return 0;
 }
