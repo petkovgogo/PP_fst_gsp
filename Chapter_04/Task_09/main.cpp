059ed58cdd4a98b3ef07b7657d416928d39eb8d6
petkovgogo <petkovgogo@gmail.com>
2020-12-04 09:58:30 +0200

    Added Chapter 4 Task 9 #54

---
+++ Chapter_04/Task_09/main.cpp
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+void func(float num)
+{
+    printf("%.2f\n", num);
+}
+
+int main()
+{
+    func(6.63);
+
+    return 0;
+}
---
+++ Chapter_04/Task_09/testFunc.cpp
@@ -0,0 +1,6 @@
+#include "stdio.h"
+
+void func(int num)
+{
+    printf("%d\n", num);
+}
