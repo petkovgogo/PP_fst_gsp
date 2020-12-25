fa9d63a0c856f0db227b7ee69b2955081cbcc229
petkovgogo <petkovgogo@gmail.com>
2020-12-25 15:47:42 +0200

    Added Chapter 6 Task 8 #92

---
+++ Chapter_06/Task_08/inc/Stack2.h
@@ -0,0 +1,21 @@
+#ifndef STACK2_H
+#define STACK2_H
+
+class Stack
+{
+    struct Link
+    {
+        void *data;
+        Link *next;
+        Link(void *dat, Link *nxt);
+        ~Link();
+    } * head;
+
+public:
+    Stack();
+    ~Stack();
+    void push(void *dat);
+    void *peek();
+    void *pop();
+};
+#endif // STACK2_H
