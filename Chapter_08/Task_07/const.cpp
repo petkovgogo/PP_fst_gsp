925b74242db16ccfd603da6135ff1bd65151a3fc
petkovgogo <petkovgogo@gmail.com>
2020-12-30 17:34:29 +0200

    Added Chapter 8 Task 7 #112

---
+++ Chapter_08/Task_07/const.cpp
@@ -0,0 +1,1 @@
+extern const int NUM = 42;
---
+++ Chapter_08/Task_07/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+extern const int NUM;
+
+int main()
+{
+    std::cout << "NUM: " << NUM << std::endl; // works fine
+
+    return 0;
+}
