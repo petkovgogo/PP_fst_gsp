c52d3a6290fb02eed865621d0c6f2d7a39a0212a
petkovgogo <petkovgogo@gmail.com>
2021-01-04 12:34:23 +0200

    Added Chapter 8 Task 22 #127

---
+++ Chapter_08/Task_22/constMember.cpp
@@ -0,0 +1,22 @@
+class X
+{
+    int i;
+
+public:
+    X(int ii);
+    int f() const;
+};
+
+X::X(int ii) : i(ii) {}
+// int X::f() { return i; }
+int X::f() const { return i; }
+
+int main()
+{
+    X x1(10);
+    const X x2(20);
+    x1.f();
+    x2.f();
+
+    return 0;
+}
