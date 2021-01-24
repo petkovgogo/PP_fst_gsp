240b64a8a97ee388ab7ca25c05974f73cb663bb2
petkovgogo <petkovgogo@gmail.com>
2021-01-24 13:01:58 +0200

    Added Chapter 10 Task 11 #200

---
+++ Chapter_11/Task_11/main.s
@@ -0,0 +1,184 @@
+	.file	"main.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+.LC0:
+	.string	"Char: "
+.LC1:
+	.string	"\nInteger: "
+.LC2:
+	.string	"\nDouble: "
+.LC3:
+	.string	"\nFloat: "
+	.text
+	.globl	_Z5printcidf
+	.type	_Z5printcidf, @function
+_Z5printcidf:
+.LFB1522:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, %eax
+	movl	%esi, -8(%rbp)
+	movsd	%xmm0, -16(%rbp)
+	movss	%xmm1, -20(%rbp)
+	movb	%al, -4(%rbp)
+	leaq	.LC0(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movsbl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_c@PLT
+	leaq	.LC1(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	movq	%rdx, %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC2(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movq	-16(%rbp), %rax
+	movq	%rax, %xmm0
+	movq	%rdx, %rdi
+	call	_ZNSolsEd@PLT
+	leaq	.LC3(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	movq	%rax, %rdx
+	movl	-20(%rbp), %eax
+	movd	%eax, %xmm0
+	movq	%rdx, %rdi
+	call	_ZNSolsEf@PLT
+	movq	%rax, %rdx
+	movq	_ZSt4endlIcSt11char_traitsIcEERSt13basic_ostreamIT_T0_ES6_@GOTPCREL(%rip), %rax
+	movq	%rax, %rsi
+	movq	%rdx, %rdi
+	call	_ZNSolsEPFRSoS_E@PLT
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1522:
+	.size	_Z5printcidf, .-_Z5printcidf
+	.globl	main
+	.type	main, @function
+main:
+.LFB1523:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	.LC4(%rip), %rax	# creating the float (?)
+	movss	.LC5(%rip), %xmm1	# passing the double
+	movq	%rax, %xmm0			# passing the float
+	movl	$451, %esi			# pssing the int
+	movl	$42, %edi			# passing the char
+	call	_Z5printcidf
+	movl	$0, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1523:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB2015:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L6
+	cmpl	$65535, -8(%rbp)
+	jne	.L6
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L6:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2015:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I__Z5printcidf, @function
+_GLOBAL__sub_I__Z5printcidf:
+.LFB2016:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE2016:
+	.size	_GLOBAL__sub_I__Z5printcidf, .-_GLOBAL__sub_I__Z5printcidf
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z5printcidf
+	.section	.rodata
+	.align 8
+.LC4:
+	.long	3092376453
+	.long	1075479838
+	.align 4
+.LC5:
+	.long	1078523331
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
