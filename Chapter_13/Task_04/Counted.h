2cbe4c86cb579c5f1dc649b23b2bc5be138461bd
petkovgogo <petkovgogo@gmail.com>
2021-02-05 15:49:23 +0200

    Added Chapter 13 Task 4 #252

---
+++ Chapter_13/Task_04/Counted.h
@@ -0,0 +1,36 @@
+#ifndef COUNTED_H
+#define COUNTED_H
+
+#include <iostream>
+
+class Counted
+{
+private:
+    static int count;
+    int m_id;
+
+public:
+    Counted();
+    ~Counted();
+
+    friend std::ostream &operator<<(std::ostream &out, Counted &countedObj);
+};
+
+int Counted::count;
+
+inline Counted::Counted() : m_id(count++)
+{
+    std::cout << "Counted object #" << m_id << " created." << std::endl;
+}
+
+inline Counted::~Counted()
+{
+    std::cout << "Counted object #" << m_id << " destroyed." << std::endl;
+}
+
+inline std::ostream &operator<<(std::ostream &out, Counted &countedObj)
+{
+    return out << "Id: " << countedObj.m_id;
+}
+
+#endif // COUNTED_H
