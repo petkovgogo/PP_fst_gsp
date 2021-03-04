78a612f4c51584c0ac979a666937797aaa6acf81
petkovgogo <petkovgogo@gmail.com>
2021-03-04 14:20:59 +0200

    Added Chapter 16 Task 10 #335

---
+++ Chapter_16/Task_10/ValueStackTest.cpp
@@ -0,0 +1,25 @@
+#include "ValueStack.h"
+#include "SelfCounter.h"
+#include <iostream>
+
+int main()
+{
+    Stack<SelfCounter> sc;
+
+    const int COUNT = 50;
+
+    for (int i = 0; i < COUNT; i++)
+    {
+        sc.push(SelfCounter());
+    }
+
+    // OK to peek(), result is a temporary:
+    std::cout << sc.peek() << std::endl;
+
+    for (int k = 0; k < COUNT; k++)
+    {
+        std::cout << sc.pop() << std::endl;
+    }
+
+    return 0;
+}
